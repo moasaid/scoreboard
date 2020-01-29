a3669db8e0477e75554150d46ca20c452262feaf
msaid <msaid@tribepad.com>
2020-01-29 14:08:02 +0000

    Reset Player Name

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -15,10 +15,10 @@ class AddPlayerForm extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.addPlayer(this.state.value);
+        this.setState({ value: ''});
     }
 
     render() {
-        console.log(this.state.value)
         return (
             <form onSubmit={this.handleSubmit}>
                 <input
